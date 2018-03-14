cc8133e65df3ba8cbff89a5506c019ce79589770
DESKTOP-OA6RQKK\The Hero Dood! <jeremy.samuelson@techfield.us>
2018-03-14 13:50:40 -0400

    Changed range to xrange, which is more appropriate for Python2

--- Python2/Arcade/Intro/adjacentElementsProduct.py
+++ Python2/Arcade/Intro/adjacentElementsProduct.py
@@ -1,2 +1,2 @@
 def adjacentElementsProduct(inputArray):
-    return max([inputArray[i] * inputArray[i+1] for i in range(len(inputArray)-1)])
+    return max([inputArray[i] * inputArray[i+1] for i in xrange(len(inputArray)-1)])
