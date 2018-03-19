0540cd1f6bfb221bae7f43082e671dbd5a9a49ce
DESKTOP-OA6RQKK\The Hero Dood! <jeremy.samuelson@techfield.us>
2018-03-19 18:27:13 -0400

    Deleted extra white space

--- Python3/Arcade/Intro/Diving Deeper/arrayMaxConsecutiveSum.py
+++ Python3/Arcade/Intro/Diving Deeper/arrayMaxConsecutiveSum.py
@@ -1,8 +1,6 @@
 def arrayMaxConsecutiveSum(a, k):
     crnt_sum = max_sum = sum(a[:k])
-    
     for i in range(len(a) - k):
         crnt_sum = crnt_sum + a[i + k] - a[i]
         max_sum = max(crnt_sum, max_sum)
-        
     return max_sum
