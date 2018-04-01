788678fdb0d803142a2e36c8e00a90cf734d7440
DESKTOP-OA6RQKK\The Hero Dood! <jeremy.samuelson@techfield.us>
2018-03-31 19:03:59 -0700

    Slight update to algorithm.

--- Python3/Arcade/Intro/Diving Deeper/arrayMaxConsecutiveSum.py
+++ Python3/Arcade/Intro/Diving Deeper/arrayMaxConsecutiveSum.py
@@ -1,6 +1,6 @@
-def arrayMaxConsecutiveSum(a, k):
-    crnt_sum = max_sum = sum(a[:k])
-    for i in range(len(a) - k):
-        crnt_sum = crnt_sum + a[i + k] - a[i]
+def arrayMaxConsecutiveSum(inputArray, k):
+    crnt_sum = max_sum = sum(inputArray[:k])
+    for i in range(len(inputArray) - k):
+        crnt_sum = crnt_sum + inputArray[i + k] - inputArray[i]
         max_sum = max(crnt_sum, max_sum)
     return max_sum
